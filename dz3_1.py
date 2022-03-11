3b5c813d922db2dbaf8de9ff4c4dcd7a9937e2b6
Alexandr Borodiy <alexander44@bk.ru>
2022-03-11 13:31:09 +0300

    lesson3_1

---
+++ dz3_1.py
@@ -0,0 +1,18 @@
+word_dict = {
+    "one": "один",
+    "two": "два",
+    "three": "три",
+    "four": "четыре",
+    "five": "пять",
+    "six": "шесть",
+    "seven": "семь",
+    "eight": "восемь",
+    "nine": "девять",
+}
+
+
+def num_translate(num_word):
+    print(word_dict.get(num_word))
+
+
+num_translate('nine')
